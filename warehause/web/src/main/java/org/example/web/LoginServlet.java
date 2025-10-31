816224de16f0d842dcf3a81963470114ceb2a283
danielberes5 <abduldan69@gmail.com>
2025-10-31 12:19:48 +0100

    2.feladatsor

--- warehause/web/src/main/java/org/example/web/LoginServlet.java
+++ warehause/web/src/main/java/org/example/web/LoginServlet.java
@@ -1,30 +1,22 @@
-package org.example;
+package org.example.web;
+import java.io.*;
 
-import java.io.IOException;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.*;
 
-@WebServlet("/login")
+//@WebServlet(name = "loginServlet", value = "/login-servlet")
 public class LoginServlet extends HttpServlet {
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        if ("admin".equals(username) && "password".equals(password)) {
-            HttpSession session = request.getSession();
-            session.setAttribute("authenticated", true);
-
-            response.sendRedirect(request.getContextPath() + "/secured/profile.html");
-        } else {
-            response.sendRedirect(request.getContextPath() + "/login.html");
+    private String message;
+    public void init() {
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        if(request.getParameter("username").equals("admin") && request.getParameter("password").equals("password")){
+            session.setAttribute("authenticated","true");
+            response.sendRedirect(response.encodeRedirectURL("Secured/profile.html"));
+        }else {
+            session.setAttribute("request","Incorrect username or password");
+            response.sendRedirect("login.html");
         }
     }
 }
