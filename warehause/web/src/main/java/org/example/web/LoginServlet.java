33672d2f50db772c6cbfec498f7a209f689379df
danielberes5 <abduldan69@gmail.com>
2025-10-31 12:19:38 +0100

    2.feladatsor

---
+++ warehause/web/src/main/java/org/example/web/LoginServlet.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("admin".equals(username) && "password".equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("authenticated", true);
+
+            response.sendRedirect(request.getContextPath() + "/secured/profile.html");
+        } else {
+            response.sendRedirect(request.getContextPath() + "/login.html");
+        }
+    }
+}
